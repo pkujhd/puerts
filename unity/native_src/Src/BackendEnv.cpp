3b0fcb0946194b52562e7486994a15a7fb92a187
jhd <jhd@babeltime.com>
2025-12-09 16:10:24 +0800

    fix compile error

--- unity/native_src/Src/BackendEnv.cpp
+++ unity/native_src/Src/BackendEnv.cpp
@@ -223,7 +223,7 @@ void FBackendEnv::GlobalPrepare()
     if (!GPlatform)
     {
         std::string Flags = "--stack_size=856";
-#if PUERTS_DEBUG
+#ifdef PUERTS_DEBUG
         Flags += " --expose-gc";
 #endif
 #if defined(PLATFORM_IOS) || defined(PLATFORM_OHOS) || defined(JITLESS)
